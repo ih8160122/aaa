960eab26323d735781da76a8868f4ee5a5bff531
sky <ih81660122@gmail.com>
2024-10-24 16:55:28 +0900

    first commlit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$("#fullpage").fullpage({
+  // sectionsColor: ["#1bbc9b", "#4BBFC3", "#7BAABE", "whitesmoke", "#ccddff"],
+  // anchors: ["firstPage", "secondPage", "3rdPage", "4thpage", "lastPage"],
+  // menu: "#menu",
+});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var swiper1 = new Swiper(".mySwiper1", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
